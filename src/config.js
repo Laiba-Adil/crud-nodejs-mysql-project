e2f97ca78dea7e81bdf16fc9a94072ee352cef31
Ubuntu <ubuntu@ip-172-31-24-70.eu-north-1.compute.internal>
2026-04-11 08:09:45 +0000

    Initial commit - Node + MySQL Docker project

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "testdb"
+};
+export const port = process.env.PORT || 3000;
---
+++ src/db.js
@@ -0,0 +1,3 @@
+import { createPool } from "mysql2/promise";
+import { config } from "./config.js";
+export const pool = createPool(config);
